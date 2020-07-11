de09072507d0b5fcf01448a4bdb80e64ae8130c3
Wasin Passornpakorn <pp.wasin@gmail.com>
2020-07-11 15:59:38 +0700

    initial view kit

---
+++ libs/CasaViewKit/CasaViewKit/CasaViewKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CasaViewKitApp.swift
+//  CasaViewKit
+//
+//  Created by Wasin Passornpakorn on 11/7/20.
+//
+
+import SwiftUI
+
+@main
+struct CasaViewKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
